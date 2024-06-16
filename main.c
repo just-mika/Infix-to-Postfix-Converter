ed574e8492f2494f3cdf3421574e9a492bebc49d
ehcac <151941275+ehcac@users.noreply.github.com>
2024-06-16 23:07:40 +0800

    Update main.c
    added evaluate postfix

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include "infix-to-postfix.c"
-//#include "evaluate-postfix.c"
+#include "evaluate-postfix.c"
 
 
 int main() {
 	String input;
+	PostFixQ* postfix;
 	
 	do
 	{
 		scanf("%255s", input);
 	
 		if(strcmp(input, "QUIT") != 0){
-			InfixToPostfix(input);
-			printQueue(); //Print postfix
+			postfix = InfixToPostfix(input);
+			printQueue(postfix);
+
+			evaluatePostFix(postfix);
+			printf("\n");
 		}
 
 	} while(strcmp(input, "QUIT") != 0);
