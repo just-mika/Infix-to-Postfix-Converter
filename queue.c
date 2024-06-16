58911304966bea4b730d10204e07a929fa2895aa
ehcac <151941275+ehcac@users.noreply.github.com>
2024-06-16 22:21:33 +0800

    Update queue.c
    removed global variables
    added initializeQueue
    changed everything that used global var before

--- queue.c
+++ queue.c
@@ -1,30 +1,26 @@
 #include "queue.h"
 
-//global variables to store addresses of front and rear nodes
-PostFixQ* pFront = NULL;
-PostFixQ* pRear = NULL;
-
-/*
 PostFixQ* initializeQueue(){
     return NULL;
 }
-*/
+
 
 /*
     checks if queue is empty
     @param *pFront - first node in the queue
-    @param *pRear` - last node in the queue
 */
-int QisEmpty(PostFixQ* pFront, PostFixQ* pRear){
-    return pFront == NULL && pRear == NULL;
+int QisEmpty(PostFixQ* pFront){
+    return pFront == NULL;
 
 }
 
 /*
     adds/enqueues operator to the rear of the queue
-    @param opt[] - operator
+    @param **pFront - head node
+    @param **pTail  - tail node, value changes to opt[]
+    @param opt[]    - operator
 */
-void enQueueOperator(char opt[]){
+void enQueueOperator(PostFixQ** pFront, PostFixQ** pRear, char opt[]){
     PostFixQ* temp = (PostFixQ*)malloc(sizeof(PostFixQ));
     if(temp == NULL){
         printf("Memory allocation failed\n");
@@ -32,15 +28,15 @@ void enQueueOperator(char opt[]){
         temp->dataType = OPERATOR;
         strcpy(temp->operator, opt);
         temp->next = NULL;
-        if(QisEmpty(pFront, pRear)){
-            pFront = temp;
-            pRear = temp;
+        if(QisEmpty(*pFront)){
+            *pFront = temp;
+            *pRear = temp;
             //pFront and pRear will contain the same value
             //queue will have only one element after this
         }else{
             //cases where size of queue prior to function call is > 0
-            pRear->next = temp; 
-            pRear = temp; //value of temp will be stored to last node/rear in queue
+            (*pRear)->next = temp; 
+            *pRear = temp; //value of temp will be stored to last node/rear in queue
         }
     }
 
@@ -48,9 +44,11 @@ void enQueueOperator(char opt[]){
 
 /*
     adds/enqueues operand to the rear of the queue
-    @param opr  - operand
+    @param **pFront - head node
+    @param **pRear  - tail node, value changes to opr
+    @param opr      - operand to be added to queue
 */
-void enQueueOperand(int opr){
+void enQueueOperand(PostFixQ** pFront, PostFixQ** pRear, int opr){
     PostFixQ* temp = (PostFixQ*)malloc(sizeof(PostFixQ));
     if(temp == NULL){
         printf("Memory allocation failed.\n");
@@ -58,12 +56,12 @@ void enQueueOperand(int opr){
         temp->dataType = OPERAND;
         temp->operand = opr;
         temp->next = NULL;
-        if(QisEmpty(pFront, pRear)){
-            pFront = temp;
-            pRear = temp;
+        if(QisEmpty(*pFront)){
+            *pFront = temp;
+            *pRear = temp;
         }else{
-            pRear->next = temp;
-            pRear = temp;
+            (*pRear)->next = temp;
+            *pRear = temp;
         }
     }
 }
@@ -72,28 +70,29 @@ void enQueueOperand(int opr){
 /*
     removes/dequeues the first node in queue -- pFront 
     then assigns the next node to pFront
+    @param **pFront - value at head, value updates to the next node
 */
-void deQueue(){
-    PostFixQ* temp = pFront;
+void deQueue(PostFixQ** pFront){
+    PostFixQ* temp = *pFront;
     //if queue is empty
-    if(QisEmpty(pFront, pRear)){
+    if(QisEmpty(*pFront)){
         printf("QUEUE IS EMPTY");
     }
     //if there's only one node left in the queue
-    if(pFront == pRear){
+    if((*pFront)->next == *pFront){
         pFront = NULL;
-        pRear = NULL;
         //after function call, queue will be empty
     }else{
-        pFront = pFront->next;
+        *pFront = (*pFront)->next;
     }
     free(temp); //deallocates dynamically allocated memory
 }
 
 /*
     prints queue
+    @param *pFront  - head node of queue to be printed
 */
-void printQueue() {
+void printQueue(PostFixQ* pFront) {
 	PostFixQ* temp = pFront;
 	while(temp != NULL) {
         //check if operator and operand
@@ -106,40 +105,3 @@ void printQueue() {
 	}
 	printf("\n");
 }
-
-
-/*
-in case you want to recheck
-int main(){
-    char input[3];
-    int isOperand;
-    int operand;
-    char operator[3];
-    while(strcmp(input, "x") != 0){
-        printf("try:");
-        scanf("%s", input);
-        isOperand = atoi(input);
-        if(input == 0 || isOperand){
-            printf("Operand");
-            operand = isOperand;
-            printf("\n%d", operand);
-            enQueueOperand(operand);
-            printf("\n");
-            printQueue();
-        }else if(strcmp(input,"d") == 0){
-            deQueue();
-            printQueue();
-        }
-        else if(!isOperand && (strcmp(input,"d") != 0)){
-            printf("Operator\n");
-            strcpy(operator, input);
-            printf("%s\n", operator);
-            enQueueOperator(operator);
-            printQueue();
-        }
-    }
-    printQueue();
-
-    return 0;
-}
-*/
